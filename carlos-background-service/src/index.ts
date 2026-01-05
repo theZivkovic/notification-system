9fe6f380542358d1cb70fdacd120a1e45256746d
Dejan Zivkovic <dejan.zivkovic90@gmail.com>
2026-01-05 13:53:26 +0100

    add delays so everything looks nicer

--- carlos-background-service/src/index.ts
+++ carlos-background-service/src/index.ts
@@ -4,7 +4,7 @@ import {BlueBookEntryStatus} from "./blueBookEntry.js";
 import {dispatchMessages} from "./messageService.js";
 
 const task = cron.schedule(
-  "* * * * *",
+  "*/5 * * * * *", // every 5 seconds for demo purposes
   async () => {
     await cronTick();
     const now = new Date().toISOString();
@@ -23,7 +23,14 @@ async function cronTick() {
     blueBookEntriesToProcess.map((entry) => entry.id),
     BlueBookEntryStatus.TAKEN_BY_CARLO
   );
+
+  await waitFor(4000); // simulate processing time
+
   await dispatchMessages(blueBookEntriesToProcess);
+}
+
+function waitFor(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 task.start();
